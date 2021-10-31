52f3e525ea0952c8ccaf82a0e2f3fd3b7812bb12
Tushar <tusharar091@gmail.com>
2021-10-31 13:44:37 +0530

    Personalized React Boilerplate App. (#1)
    * Added link to github profile
    
    * Added name
    
    * Updated readme
    
    Co-authored-by: Tushar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, My name is <code>Tushar</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/tusharar091"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See my Github Profile
         </a>
       </header>
     </div>
