2f407d328cac04c660136ca4c06eeafd3e9e1278
Tushar <tushar@ingenuitygaming.com>
2021-11-01 17:47:58 +0530

    Created GroceryForm component using which user can add item

---
+++ src/components/GroceryForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const GroceryForm = ({ addItem }) => {
+    const [item, setItem] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addItem(item);
+        setItem('');
+    }
+    return (
+        <div>
+            <p>Shopping List</p>
+            <form onSubmit={handleSubmit}>
+                <div className="form-grp">
+                    <input className="field"
+                        placeholder="Name"
+                        name="name"
+                        id="name"
+                        value={item}
+                        onChange={(e) => setItem(e.target.value)}
+                    />
+                    <label for="name" class="label">
+                        Item Name
+                    </label>
+                </div>
+                <br />
+                <button className="myButton">
+                    {" "}
+                    Add in Bag
+                    <span role="img" aria-label="bag">
+                        {" "}
+                        🛍️
+                    </span>
+                </button>
+            </form>
+        </div>
+    )
+}
