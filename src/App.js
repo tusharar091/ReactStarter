10214f212f323e0357e3e76fd5c5def02b2a3779
Tushar <tusharar091@gmail.com>
2021-11-07 23:32:00 +0530

    Grocery List Application [Fixes & Enhancements] (#8)
    * [Issue No #4] List items aligned to the left
    
    * [Issue No #5] Delete Button Alignment Corrected
    
    * [Issue No #7] Add Button Minimalization completed
    
    * Quantified grocery list items
    
    * [Issue No #6] Addition of Quantity Counter || Duplicate Key insertion now increases quantity instead of new item insertion

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import GroceryList from './components/GroceryList';
 import GroceryForm from './components/GroceryForm';
 function App() {
   const savedItems = JSON.parse(localStorage.getItem('items'));
+  const savedQuantity = JSON.parse(localStorage.getItem("quantity"));
   const [items, setItems] = useState(savedItems || []);
+  const [quantity, setQuantity] = useState(savedQuantity || []);
 
   const addItem = (item) => {
+    if (items.includes(item)) {
+      updateQuantity(null, item, true);
+      return;
+    }
     setItems([...items, item]);
+    initQuantity();
+  }
+  const initQuantity = () => {
+    setQuantity([...quantity, 1]);
   }
   const removeItem = (itemToBeDeleted) => {
     setItems(items.filter(item => item !== itemToBeDeleted));
+    removeQuantity(itemToBeDeleted);
+  }
+  const removeQuantity = itemToBeDeleted => {
+    setQuantity(quantity.filter((quant, index) => index !== items.indexOf(itemToBeDeleted)));
+  }
+  const updateQuantity = (value, item, increment = false) => {
+    const index = items.indexOf(item);
+    quantity[index] = increment ? quantity[index] + 1 : value;
+    setQuantity([...quantity]);
   }
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("items"));
@@ -21,10 +40,19 @@ function App() {
   useEffect(() => {
     localStorage.setItem('items', JSON.stringify(items));
   }, [items])
+  useEffect(() => {
+    localStorage.setItem('quantity', JSON.stringify(quantity));
+  }, [quantity])
+  useEffect(() => {
+    const quantity = JSON.parse(localStorage.getItem("quantity"));
+    if (quantity) {
+      setQuantity(quantity)
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <GroceryList items={items} removeItem={removeItem} />
+        <GroceryList items={items} quantity={quantity} updateQuantity={updateQuantity} removeItem={removeItem} />
         <GroceryForm addItem={addItem} />
         Grocery Items
       </header>
