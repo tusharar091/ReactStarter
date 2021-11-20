e17981109b2a6662ae1c44eba0b2be94562728e5
Tushar <tusharar091@gmail.com>
2021-11-20 23:16:01 +0530

    Managing state from parent component Board instead of Square

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        }
-    }
     render() {
         return (
             <button className="square"
                 onClick={() => {
-                    this.setState({ value: 'X' })
+
                 }}>
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: new Array(9).fill(null)
+        }
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]} />;
     }
 
     render() {
