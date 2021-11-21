deb821157239af791d34f937c9e9e54badd601cf
Tushar <tusharar091@gmail.com>
2021-11-21 17:08:57 +0530

    Two for loops to create board in render()

--- src/index.js
+++ src/index.js
@@ -14,28 +14,28 @@ function Square(props) {
 class Board extends React.Component {
 
     renderSquare(i) {
-        return <Square value={this.props.value[i]} onClick={() => this.props.onClick(i)} />;
+        return <Square key={i} value={this.props.value[i]} onClick={() => this.props.onClick(i)} />;
     }
-
-    render() {
+    createBody() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {
+                    [0, 1, 2].map((i) => {
+                        return <div key={i} className="board-row">
+                            {
+                                [0, 1, 2].map((j) => {
+                                    return this.renderSquare((i * 3) + j);
+                                })
+                            }
+                        </div>
+                    })
+                }
             </div>
+        )
+    }
+    render() {
+        return (
+            this.createBody()
         );
     }
 }
